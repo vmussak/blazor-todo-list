ff5cd6a03b4ab5b0736258f53d89525d1f03ded3
Vinicius Mussak <mussak@smn.com.br>
2018-05-14 14:05:11 -0300

    Adicionar arquivos de projeto.

---
+++ BlazorTodoList/Models/TarefaModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTodoList.Models
+{
+    public class TarefaModel
+    {
+        public string Titulo { get; set; }
+        public bool Completa { get; set; }
+    }
+}
